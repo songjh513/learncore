29f98cf6e1ff576368574c3efbab139eec00212d
songjh <525004657@qq.com>
2019-03-23 18:28:40 +0800

    add test

--- learn-common/src/main/java/com/songjh/learncore/common/jsonschema/JsonValidatorDemo.java
+++ learn-common/src/main/java/com/songjh/learncore/common/jsonschema/JsonValidatorDemo.java
@@ -14,20 +14,45 @@ import org.everit.json.schema.Schema;
  */
 public class JsonValidatorDemo {
 
-    public static void main(String[] args){
 
-    }
 
     @Test
-    public void testJsonSchema3() {
+    public void testJsonSchema() {
         InputStream inputStream = getClass().getResourceAsStream("/schema/schemaDemo.json");
         JSONObject Schema = new JSONObject(new JSONTokener(inputStream));
-        JSONObject data = new JSONObject("{\"data\" : 1234}");
-        Schema schema = SchemaLoader.load(Schema);
+        String test = "{"+
+                    "\"street_address\": \"24 Sussex Drive\","+
+                    "\"country\": \"Canada\","+
+                    "\"postal_code1111\": \"10000\""+
+                "}";
+        JSONObject data = new JSONObject(test);
+
+        SchemaLoader loader = SchemaLoader.builder()
+                .schemaJson(Schema)
+                .draftV7Support() // or draftV7Support()
+                .build();
+        Schema schema = loader.load().build();
+
         try {
             schema.validate(data);
         } catch (ValidationException e) {
             System.out.println(e.getMessage());
+            printLog(e);
+        }
+    }
+
+    private void printLog(ValidationException e) {
+        for (ValidationException validationException : e.getCausingExceptions()) {
+            if(validationException.getCausingExceptions().size()>0){
+                for (ValidationException exception : e.getCausingExceptions()) {
+                    this.printLog(exception);
+                }
+            }else {
+                System.out.println("pointerToViolation:"+validationException.getPointerToViolation());
+                System.out.println("schemaLocation:"+validationException.getSchemaLocation());
+                System.out.println("violatedSchema:"+validationException.getViolatedSchema());
+                System.out.println("message:"+validationException.getMessage());
+            }
         }
     }
 }
